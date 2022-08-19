7ad437b6b9739973b94d8cad7cb3e67e1ad02353
Luiz Melo <programadorlesd@gmail.com>
2022-08-19 18:50:09 -0300

    inicio

---
+++ src/main/java/com/agence/global/projetoAgence/ProjetoAgenceApplication.java
@@ -0,0 +1,13 @@
+package com.agence.global.projetoAgence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAgenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAgenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agence/global/projetoAgence/ProjetoAgenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agence.global.projetoAgence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAgenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
