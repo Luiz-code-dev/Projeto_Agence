bd5609572abac110eebb2278d307d1f70e750227
Luiz Melo <programadorlesd@gmail.com>
2022-08-20 23:26:02 -0300

    Ajustes

--- src/main/java/com/agence/global/projetoAgence/repository/FuncionarioRepository.java
+++ src/main/java/com/agence/global/projetoAgence/repository/FuncionarioRepository.java
@@ -2,9 +2,7 @@ package com.agence.global.projetoAgence.repository;
 
 import com.agence.global.projetoAgence.entidades.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
